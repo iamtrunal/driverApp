4b023298110d38a0ecea6fd2245b0ec5514bd3af
Trunal <trunaldudhatra083@gmail.com>
2022-08-26 10:31:12 +0530

    CHANGE:portDefine

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8000;
-
+require("dotenv").config();
+const port = process.env.PORT || 8000;
 require("./db/conn");
 
 app.use(express.json());
