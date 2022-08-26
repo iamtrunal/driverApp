90eab71e39bc15e8bfc806dfbbb2f3ef3c372014
Trunal <trunaldudhatra083@gmail.com>
2022-08-26 19:08:31 +0530

    ADD:chatSocketAdd

--- db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://td:td_0083@cluster0.71szoqm.mongodb.net/?retryWrites=true&w=majority")
-.then(() => {
-    console.log("Database Connected")
-})
-.catch(() => {
-    console.log("Database Not Connected")
-})
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+require("./db/conn");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// ---------- Define Routes Here ---------- //
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
+module.exports = app;
