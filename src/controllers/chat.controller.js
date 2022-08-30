26d6b6da9fca1b35277765c5a91e0e6b429f5117
Trunal <trunaldudhatra083@gmail.com>
2022-08-30 10:27:12 +0530

    FIXES: chatResponseGetAllChat

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -7,49 +7,56 @@ const status = require("http-status");
 exports.getChatByUserId = async (req, res) => {
     try {
 
-        const findChatRoom = await chatRoom.findOne({
+        const findChatRoom = await chatRoom.find({
             user1: req.body.user_id
-        });
-        console.log("findChatRoom::", findChatRoom);
+        }).lean();
+        console.log("findChatRoom::", findChatRoom.length);
 
-        const findChatRoom2 = await chatRoom.findOne({
+        const findChatRoom2 = await chatRoom.find({
             user2: req.body.user_id
-        })
-        console.log("findChatRoom2::", findChatRoom2);
+        }).lean();
+        console.log("findChatRoom2::", findChatRoom2.length);
 
-        if (findChatRoom == null && findChatRoom2 == null) {
+        if (findChatRoom.length == 0 && findChatRoom2.length == 0) {
 
             res.status(status.NOT_FOUND).json(
                 new APIResponse("Data Not Exist", "false", 404, "0")
             )
 
-        } else {
+        }
+        else {
 
-            if (findChatRoom) {
+            if (findChatRoom.length > 0) {
 
-                const getUserData = await authModel.findOne(
-                    {
-                        _id: req.body.user_id
+                const response = [];
+                for (const getLastMsg of findChatRoom) {
+
+                    console.log("getLastMessage::", getLastMsg._id);
+
+                    const getUserData = await authModel.findOne(
+                        {
+                            _id: getLastMsg.user1
+                        }
+                    ).lean();
+
+                    const findChat1 = await chatModel.findOne(
+                        {
+                            chatRoomId: getLastMsg._id
+                        }
+                    ).lean();
+
+                    const chatMessage = findChat1.chat;
+                    const getLastMessage = chatMessage[chatMessage.length - 1];
+                    console.log("getLastMessage:::", getLastMessage.message);
+
+                    const lastMsgResponse = {
+                        profile: getUserData.profile[0].res,
+                        chatRoomId: findChat1.chatRoomId,
+                        username: getUserData.username,
+                        message: getLastMessage.message,
                     }
-                )
-                console.log("getUserData::", getUserData);
+                    response.push(lastMsgResponse)
 
-                const findChat1 = await chatModel.findOne(
-                    {
-                        chatRoomId: findChatRoom._id
-                    }
-                );
-                console.log("findChat1::", findChat1);
-
-                const chatMessage = findChat1.chat;
-                const getLastMessage = chatMessage[chatMessage.length - 1];
-                console.log("getLastMessage:::", getLastMessage.message);
-
-                const response = {
-                    profile: getUserData.profile[0].res,
-                    chatRoomId: findChat1.chatRoomId,
-                    username: getUserData.username,
-                    message: getLastMessage.message,
                 }
 
                 res.status(status.OK).json(
@@ -58,33 +65,38 @@ exports.getChatByUserId = async (req, res) => {
 
             } else {
 
-                const getUserData = await authModel.findOne(
-                    {
-                        _id: req.body.user_id
+                const response2 = [];
+                for (const getLastMsg2 of findChatRoom2) {
+
+                    console.log("getLastMessage::", getLastMsg2._id);
+
+                    const getUserData2 = await authModel.findOne(
+                        {
+                            _id: getLastMsg2.user2
+                        }
+                    ).lean();
+
+                    const findChat2 = await chatModel.findOne(
+                        {
+                            chatRoomId: getLastMsg2._id
+                        }
+                    ).lean();
+
+                    const chatMessage2 = findChat2.chat;
+                    const getLastMessage2 = chatMessage2[chatMessage2.length - 1];
+                    console.log("getLastMessage2:::", getLastMessage2.message);
+
+                    const lastMsgResponse2 = {
+                        profile: getUserData2.profile[0].res,
+                        chatRoomId: findChat2.findChat2,
+                        username: getUserData2.username,
+                        message: getLastMessage2.message,
                     }
-                )
-                console.log("getUserData::", getUserData);
-
-                const findChat2 = await chatModel.findOne(
-                    {
-                        chatRoomId: findChatRoom2._id
-                    }
-                );
-                console.log("findChat2::", findChat2);
-
-                const chatMessage2 = findChat2.chat;
-                const getLastMessage2 = chatMessage2[chatMessage2.length - 1];
-                console.log("getLastMessage2:::", getLastMessage2.message);
-
-                const response = {
-                    profile: getUserData.profile[0].res,
-                    chatRoomId: findChat2.findChat2,
-                    username: getUserData.username,
-                    message: getLastMessage2.message,
+                    response2.push(lastMsgResponse2)
                 }
 
                 res.status(status.OK).json(
-                    new APIResponse("Get Chat Details By UserId", true, 200, 1, response)
+                    new APIResponse("Get Chat Details By UserId", true, 200, 1, response2)
                 )
 
             }
@@ -106,11 +118,11 @@ exports.getAllChatData = async (req, res) => {
 
         const findChatRoom = await chatModel.findOne({
             chatRoomId: req.body.chat_room_id
-        });
+        }).select("-__v").lean();
         console.log("findChatRoom::", findChatRoom);
 
 
-        if(findChatRoom == null) {
+        if (findChatRoom == null) {
 
             res.status(status.NOT_FOUND).json(
                 new APIResponse("Data Not Exist", "false", 404, "0")
@@ -170,6 +182,6 @@ exports.getAllChatData = async (req, res) => {
         res.status(status.INTERNAL_SERVER_ERROR).json(
             new APIResponse("Something Went Wrong", "false", 500, "0", error.message)
         )
-        
+
     }
 }
